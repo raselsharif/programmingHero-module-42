9fff241a3998e890b2312664e287feea11a457b6
Rasel Sharif <raselsharif9@gmail.com>
2023-09-11 22:42:12 +0600

    fetch blogs data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-f import './App.css'
+import './App.css'
+import Blogs from './assets/components/Blogs/Blogs'
 
 function App() {
 
   return (
     <>
      <h1 className='text-3xl'>Knowlege Cafe</h1>
+     <Blogs></Blogs>
     </>
   )
 }
